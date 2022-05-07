bbebf6b74d4a2fd25b18f572319e4162644fbdb4
SuperDuperBlade <85508905+SuperDuperBlade@users.noreply.github.com>
2022-05-07 11:16:50 +0100

    added the base for commands

---
+++ src/main/java/Commands/Ping.java
@@ -0,0 +1,12 @@
+package Commands;
+
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+
+public class Ping extends Commands {
+
+
+
+
+
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,13 @@
 import Events.Message;
+import com.jagrosh.jdautilities.command.CommandClient;
+import com.jagrosh.jdautilities.command.CommandClientBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.RichPresence;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.security.auth.login.LoginException;
 
@@ -9,8 +16,14 @@ public class Main {
 
     public static void main(String []args) throws LoginException {
 
-        JDA jda = JDABuilder.createDefault("OTcxNDM0NjYzNTQzNDM5NDYw.GulEze.OtdxPcfIFO6IDgYadQ4ja2EGaJGlFqF9PqjPVg").build();
+        JDA jda = JDABuilder.createDefault("").build();
         jda.addEventListener(new Message());
+        CommandClientBuilder builder = new CommandClientBuilder();
+        builder.setPrefix("<");
+        builder.setOwnerId("731182000165093428");
+        builder.setActivity(Activity.playing("Currently studying"));
+        CommandClient client = builder.build();
+
 
 
 
