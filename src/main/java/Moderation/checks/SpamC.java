a124c39c1fded396fdea2e427290dd3648dabcc1
SuperDuperBlade <85508905+SuperDuperBlade@users.noreply.github.com>
2022-05-25 18:57:21 +0100

    false flag fix

--- src/main/java/Moderation/checks/SpamC.java
+++ src/main/java/Moderation/checks/SpamC.java
@@ -1,6 +1,4 @@
 package Moderation.checks;
-
-import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -9,26 +7,37 @@ import static Moderation.Moderation.flag;
 import static Moderation.Moderation.flagtype;
 
 public class SpamC extends ListenerAdapter {
-private Message messID;
+    private Message messID;
     //this checks to see if the previous message was the same as the last one
 
-        private String preMessage= "";
+    private String preMessage = "";
+    private String une = "";
 
 
     @Override
     public void onMessageReceived(MessageReceivedEvent e) {
-if(e.getAuthor().isBot())
-    return;
+        if (e.getAuthor().isBot())
+            return;
 
 
-        this.preMessage = this.preMessage.replaceAll("\\d","");
-            if (this.preMessage.equalsIgnoreCase(e.getMessage().getContentRaw().replaceAll("\\d",""))){
-                this.messID.delete().queue();
-                flag(flagtype, e.getAuthor(), e.getGuild(), e.getChannel(), "Spam C", e.getMessage());
-                e.getMessage().delete().queue();
-                return;
-            }
-            this.messID = e.getMessage();
-            preMessage = e.getMessage().getContentRaw();
+String[] amount = e.getMessage().getContentRaw().split(" ");
+
+        if (this.preMessage.equalsIgnoreCase("")&&e.getMessage().getContentRaw().replaceAll("\\d", "").equals("")&& amount.length<3){
+            return;
+        }
+
+
+
+
+
+        if (this.preMessage.equalsIgnoreCase(e.getMessage().getContentRaw().replaceAll("\\d", ""))) {
+            this.messID.delete().queue();
+            flag(flagtype, e.getAuthor(), e.getGuild(), e.getChannel(), "Spam C", e.getMessage());
+            e.getMessage().delete().queue();
+            return;
+        }
+        this.messID = e.getMessage();
+        this.une = e.getMessage().toString();
+        preMessage = e.getMessage().getContentRaw().replaceAll("\\d", "");
     }
 }
